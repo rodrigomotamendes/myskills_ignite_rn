aa2a834f79d31dddd6f23a05c23e6f45f0ebef6e
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-05-27 12:57:09 -0300

    Carregamento da Splash

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { Home } from './src/pages/Home';
-
-
+import SplashScreen from 'react-native-splash-screen'
 
 export default function App() {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#121015"/>
